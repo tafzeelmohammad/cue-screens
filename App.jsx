1271c201f942cacd0990f5ba23ebac64e965c0c2
tafzeel <tafzeel@tactconnect@gmail.com>
2022-02-09 18:01:19 +0530

    upload

--- App.jsx
+++ App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Fragment>
         <Router>
           <Switch>
-            <Route path="/:path(|home)"><Community /></Route>
+            <Route path="/:path(|home)"><Artists /></Route>
             <Route path="/signin"><SignIn /></Route>
             <Route path="/validate-otp" component={ Otp } />
             <Route path="/fill-details" component={ Details } />
